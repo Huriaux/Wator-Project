537c7209144b99472d66eefd5eabf9e16f79cdd6
Cecile HURIAUX <cecile.huriaux@gmail.com>
2024-09-15 20:33:47 +0200

    first commit : project recovery

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from simulation import Simulation, World
+import sys
+
+
+def main():
+    pygame.init()
+    world = World(50,50)
+    simulation = Simulation(world,1000, 40, 10, 50)
+    simulation.initialisation()
+
+    simulation.ending_simulation()
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
